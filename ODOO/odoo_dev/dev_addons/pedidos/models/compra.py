8bfc1e6e8107c3e9ede59ca07142ded09d684481
AlejandroGarcia77 <67334614+AlejandroGarcia77@users.noreply.github.com>
2024-03-07 19:21:05 +0100

    v 0.0.3
    -Creando módulo de pedidos.
    -Creando funcionalidad para confirmación de pedidos.
    @JuankiAvila

--- ODOO/odoo_dev/dev_addons/pedidos/models/compra.py
+++ ODOO/odoo_dev/dev_addons/pedidos/models/compra.py
@@ -39,45 +39,7 @@ class purchase_custom(models.Model):
             log_message += f"\n\tImpuestos: {product.taxes_id.name}"
             log_message += f"\n\tSubtotal: {product.price_subtotal}"
 
-        # Create a dictionary to store the log information
-        log_data = {
-            "Usuario": self.partner_id.name,
-            "Pedido": self.name,
-            "Referencia Proveedor": self.partner_ref,
-            "Moneda": self.currency_id.name,
-            "Fecha confirmación": self.date_approve,
-            "Fecha esperada": self.date_planned,
-            "Productos": []
-        }
-
-        # Loop through order lines and add product information to a list
-        for product in self.order_line:
-            product_info = {
-                "Nombre": product.product_id.name,
-                "Descripción": product.name,
-                "Cantidad": product.product_qty,
-                "Recibido": product.qty_received,
-                "Facturado": product.qty_invoiced,
-                "Precio Unitario": product.price_unit,
-                "Impuestos": product.taxes_id.name,
-                "Subtotal": product.price_subtotal
-            }
-            log_data["Productos"].append(product_info)
-
-        # Convert the dictionary to JSON (text format)
-        json_data = json.dumps(log_data)
-
-        # Encode JSON data to base64 (text format)
-        encoded_data = base64.b64encode(json_data.encode('utf-8')).decode('utf-8')
-
-        # Enviar un mensaje al tópico 'tema-prueba-ERP'
-        producer.send(self.name, encoded_data, partition=1 )
-        # Asegurarse de que todos los mensajes hayan sido enviados
-        producer.flush()
-
-        # Cerrar el productor
-        producer.close()
-
+        _logger.info(log_message)
 
         return res 
     
