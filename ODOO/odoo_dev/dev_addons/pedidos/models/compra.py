b0309beb9a490960bcff12d9f0f3b161aa5075b3
juanki600 <42650952+juanki600@users.noreply.github.com>
2024-03-09 18:25:56 +0100

    v 0.0.7
    Se ha implementado el protocolo de mensajes a través de kafka.
    
    También se sube la actualización de docker-compose
    
    Co-Authored-By: AlejandroGarcia77 <67334614+AlejandroGarcia77@users.noreply.github.com>

--- ODOO/odoo_dev/dev_addons/pedidos/models/compra.py
+++ ODOO/odoo_dev/dev_addons/pedidos/models/compra.py
@@ -3,45 +3,77 @@ from kafka import KafkaProducer
 import logging
 import json
 import base64
+from kafka.admin import KafkaAdminClient, NewTopic
+
 
 _logger = logging.getLogger(__name__)
-# Crear una instancia del productor de Kafka
-# producer = KafkaProducer(bootstrap_servers='localhost:9092',
-#                         max_block_ms=1048588,
-#                         compression_type='gzip')
-
 
 class purchase_custom(models.Model):
-
     _inherit = 'purchase.order'
 
     def button_confirm(self):
+        _logger.info("HACIENDO PRUEBAS DE QUE HAY CAMBIOS 1")
+        # Datos de configuración de Kafka
+        kafka_server = "172.22.0.5:29093"  # Ajusta según tu configuración
+        topic_name = self.name
+        _logger.info(topic_name)
 
-        res = super(purchase_custom, self).button_confirm()
+        # Crear el cliente de administración de Kafka
+        admin_client = KafkaAdminClient(
+            bootstrap_servers=kafka_server,
+            client_id='admin'
+        )
+        _logger.info("HACIENDO PRUEBAS DE QUE HAY CAMBIOS 2")
+        # Crear un objeto NewTopic
+        topic = NewTopic(name=topic_name, num_partitions=1, replication_factor=1)
 
-        # Crear un mensaje de log con los datos del usuario
-        log_message = f"Usuario: {self.partner_id}"
-        log_message += f"\nPedido: {self.name}"
-        log_message += f"\nReferencia Proveedor: {self.partner_ref}"
-        log_message += f"\nMoneda: {self.currency_id.name}"
-        log_message += f"\nFecha confirmación: {self.date_approve}"
-        log_message += f"\nFecha esperada: {self.date_planned}"     
-        log_message += f"\nFecha esperada: {self.date_planned}"
+        # Crear el tópico
+        try:
+            admin_client.create_topics(new_topics=[topic], validate_only=False)
+        except Exception as e:
+            _logger.error(f"Error al crear el tópico: {e}")
 
-        log_message += f"\nProducto:"
+        # Crear una instancia del productor de Kafka
+        producer = KafkaProducer(bootstrap_servers=kafka_server,
+                                 max_block_ms=1048588,
+                                 compression_type='gzip')
+        log_data = {
+            "Usuario": self.partner_id.name,
+            "Pedido": self.name,
+            "Referencia Proveedor": self.partner_ref,
+            "Moneda": self.currency_id.name,
+            "Fecha esperada": self.date_planned.strftime('%Y-%m-%d'),
+            "Productos": []
+        }
+
+        # Loop through order lines and add product information to a list
         for product in self.order_line:
-            log_message += f"\n\tNombre: {product.product_id.name}"
-            log_message += f"\n\tDescripción: {product.name}"
-            log_message += f"\n\tCantidad: {product.product_qty}"
-            log_message += f"\n\tRecibido: {product.qty_received}"
-            log_message += f"\n\tFacturado: {product.qty_invoiced}"
-            log_message += f"\n\tPrecio Unitario: {product.price_unit}"
-            log_message += f"\n\tImpuestos: {product.taxes_id.name}"
-            log_message += f"\n\tSubtotal: {product.price_subtotal}"
+            product_info = {
+                "Nombre": product.product_id.name,
+                "Descripción": product.name,
+                "Cantidad": product.product_qty,
+                "Recibido": product.qty_received,
+                "Facturado": product.qty_invoiced,
+                "Precio Unitario": product.price_unit,
+                "Impuestos": product.taxes_id.name,
+                "Subtotal": product.price_subtotal
+            }
+            log_data["Productos"].append(product_info)
 
-        _logger.info(log_message)
+        # Convertir los datos a JSON y codificarlos en base64
+        json_data = json.dumps(log_data)
+        encoded_data = base64.b64encode(json_data.encode('utf-8')).decode('utf-8')
 
-        return res 
-    
+        # Enviar el mensaje al tópico de Kafka
+        try:
+            producer.send(topic_name, encoded_data.encode('utf-8'))
+            producer.flush()
+        except Exception as e:
+            _logger.error(f"Error al enviar el mensaje a Kafka: {e}")
+        finally:
+            producer.close()
 
+        _logger.info(log_data)
+        _logger.info("HACIENDO PRUEBAS DE QUE HAY CAMBIOS 3")
 
+        return True
