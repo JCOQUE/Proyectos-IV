398779bb7c6681471dcab7909ce879f54fc25063
juanki600 <42650952+juanki600@users.noreply.github.com>
2024-04-16 22:37:29 +0200

    v 0.0.22
    Se ha arreglado el problema que corrompía el programa.
    
    Solución:
    
    Se ha creado un finaly con el Pedidos.cancel() dentro

--- ODOO/odoo_dev/dev_addons/pedidos/models/ventas.py
+++ ODOO/odoo_dev/dev_addons/pedidos/models/ventas.py
@@ -1,4 +1,4 @@
-from odoo import models, api,registry
+from odoo import models, api, registry
 from kafka import KafkaConsumer
 import logging
 import threading
@@ -17,7 +17,7 @@ class KafkaConsumerSaleOrder(models.TransientModel):
         PEDIDOS = None
         try:
             kafka_server = '172.26.0.4:29093'
-            topic_name = 'P00004'
+            topic_name = 'P00002'
             PEDIDOS = KafkaConsumer(
                 topic_name,
                 bootstrap_servers=kafka_server,
@@ -28,27 +28,23 @@ class KafkaConsumerSaleOrder(models.TransientModel):
                 pedido_str = mensaje.value.decode('utf-8')
                 pedido_dict = json.loads(pedido_str)
                 if str(pedido_dict['sender_ip']) != str(local_ip):
-                    loggerC.critical(pedido_dict)
                     pedido_dict.pop('sender_ip', None)
                     loggerC.critical(pedido_dict)
                     loggerC.critical('MENSAJE RECIBIDO CONSUMER')
-                loggerC.critical('CHUPA PIJA')
-                PEDIDOS.close()
-            loggerC.critical('CHUPA PIJA2')
+                    try:
+                        api.Environment.manage()
+                        
+                        with registry(self._cr.dbname).cursor() as new_cr:
+                            self = self.with_env(self.env(cr=new_cr)).with_context(original_cr=self._cr)
+                            nuevo_pedido = self.env['sale.order'].sudo().create(pedido_dict)
+                            new_cr.commit()
+                    except Exception as e:
+                        loggerC.error(f"Error al mandar mensaje: {e}")
         except Exception as e:
             loggerC.error(f"Error en el consumidor: {e}")
-        try:
-            loggerC.critical("Antes de insertar")
-            api.Environment.manage()
-
-            new_cr = registry(self._cr.dbname).cursor()
-            new_cr.autocommit(True)
-            self = self.with_env(self.env(cr=new_cr)).with_context(original_cr=self._cr)
-            nuevo_pedido = self.env['sale.order'].sudo().create(pedido_dict)
-            loggerC.critical("Pedido insertado")
-        except Exception as e:
-            loggerC.error(f"Error al mandar mensaje: {e}")
-
+        finally:
+            if PEDIDOS:
+                PEDIDOS.close()
 
     def start_consumer_thread(self):
         threaded_calculation = threading.Thread(target=self.consume_messages)
@@ -58,3 +54,4 @@ class KafkaConsumerSaleOrder(models.TransientModel):
     def init(self):
         super(KafkaConsumerSaleOrder, self).init()
         self.start_consumer_thread()
+
