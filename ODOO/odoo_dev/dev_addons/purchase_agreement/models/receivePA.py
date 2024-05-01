4646b14e43638c044f591ece90d3f60d92946556
Javier <jcoque81@gmail.com>
2024-05-01 12:41:18 +0200

    v 2.6
    Implementación purchase agreement en ambos odoos
    
    Creaacion de un topico para purchase  agreement y que vaya por distinto  canal al de compras
    
    Hay un error minimo de que se manda el mensaje dos veces. No me preocupa

---
+++ ODOO/odoo_dev/dev_addons/purchase_agreement/models/receivePA.py
@@ -0,0 +1,80 @@
+from odoo import models, api, registry
+from kafka import KafkaConsumer
+import time
+import logging
+import threading
+import socket
+import json
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+import hashlib
+
+loggerC = logging.getLogger('consumer')
+
+class receivePA(models.TransientModel):
+    _name = 'receive.pa'
+    __key = hashlib.sha256('admin123'.encode('utf-8')).digest()
+    __iv =  hashlib.sha256('admin123'.encode('utf-8')).digest()[:16]
+
+    def init(self):
+        super(receivePA, self).init()
+        self.start_consumer_thread()
+
+    def start_consumer_thread(self):
+        threaded_calculation = threading.Thread(target=self.consume_messages)
+        threaded_calculation.daemon = True
+        threaded_calculation.start()
+
+    @api.model
+    def consume_messages(self):
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        try:
+            PEDIDOS = self.connect_kafka()
+            for consumer_record in PEDIDOS:
+                loggerC.critical('Mensaje recibido')
+                mensaje_encoded = consumer_record.value
+                mensaje = self.decode_message(mensaje_encoded)
+                loggerC.critical(mensaje)
+                if str(mensaje['sender']) == str('Amazon'):
+                    loggerC.critical('Processing message...')
+                    self.read_message(mensaje)
+        except Exception as e:
+            loggerC.error(f"Error en el consumidor: {e}")
+        finally:
+            if PEDIDOS:
+                PEDIDOS.close()
+
+    def connect_kafka(self):
+        kafka_server = '192.168.0.33:31234'
+        topic_name = 'PA'
+        msg = KafkaConsumer(
+            topic_name,
+            bootstrap_servers=[kafka_server],
+            auto_offset_reset='latest',
+            enable_auto_commit=True,
+        )
+        return msg
+
+    def decode_message(self, mensaje_encryted):
+        #Desencriptacion
+        cipher = Cipher(algorithms.AES(self.__key), modes.CBC(self.__iv))
+        decryptor = cipher.decryptor()
+        decrypted_padded_data = decryptor.update(mensaje_encryted) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        unpadded_data = unpadder.update(decrypted_padded_data) + unpadder.finalize()
+        mensaje_decryted = json.loads(unpadded_data.decode('utf-8'))
+        
+        return mensaje_decryted
+
+    def read_message(self, pedido):
+        pedido.pop('sender_ip', None)
+        loggerC.critical(pedido)
+        loggerC.critical('MENSAJE RECIBIDO CONSUMER')
+        # try:
+        #     self.add_sale_record(pedido)
+        # except Exception as e:
+        #     loggerC.error(f"Error al mandar mensaje: {e}")
+
+
+    
