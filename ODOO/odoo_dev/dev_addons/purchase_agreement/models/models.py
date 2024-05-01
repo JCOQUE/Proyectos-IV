3ef105cc578f43cbf74708894e9e387932f8cb37
Javier <jcoque81@gmail.com>
2024-05-01 11:11:39 +0200

    2.4
    Creacion boton purchase y su funcion asociada

--- ODOO/odoo_dev/dev_addons/purchase_agreement/models/models.py
+++ ODOO/odoo_dev/dev_addons/purchase_agreement/models/models.py
@@ -1,9 +1,12 @@
 from odoo import models, fields, api
+import logging 
 
-
+_logger = logging.getLogger('producer')
 class purchase_agreement(models.Model):
     _name = 'purchase_agreement.purchase_agreement'
     _description = 'purchase_agreement.purchase_agreement'
 
     name = fields.Char()
     
+    def send_pa(self):
+        _logger.critical('PUUUUUUUURCHAAAAAAAAAAAAAAASE')
